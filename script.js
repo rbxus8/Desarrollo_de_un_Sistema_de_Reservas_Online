6057a3ae47e7f08accb1e699864750a4eb0344f5
julian carvajalino <juliancarvajalino8@gmail.com>
2025-04-12 10:01:24 -0500

    header agrego opcion js

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const fotos = document.querySelectorAll(".pers_foto img");
-  const imagenPrincipal = document.getElementById("imagen_principal");
-  const nombre = document.getElementById("nombre");
-  const cargo = document.getElementById("cargo");
-  const bio = document.getElementById("bio");
+window.addEventListener("resize", () => {
+  const checkbox = document.getElementById("menu_hamb");
 
-  fotos.forEach((foto) => {
-    foto.addEventListener("click", function () {
-      const nombreTexto = this.getAttribute("data-nombre");
-      const cargoTexto = this.getAttribute("data-cargo");
-      const bioTexto = this.getAttribute("data-bio");
-
-      // Cambia la imagen principal y muestra la información correspondiente
-      imagenPrincipal.src = this.src;
-      nombre.textContent = `Nombre: ${nombreTexto}`;
-      cargo.textContent = `Cargo: ${cargoTexto}`;
-      bio.textContent = `Descripción: ${bioTexto}`;
-    });
-  });
+  // Si el ancho es mayor a 768px, desmarcamos el menú
+  if (window.innerWidth > 768 && checkbox.checked) {
+    checkbox.checked = false;
+  }
 });
