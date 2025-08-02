a9fae3fff9ec21fbd1ced4f662ae5026c001fb1e
julian carvajalino <juliancarvajalino8@gmail.com>
2025-08-01 23:28:33 -0500

    feat: login

---
+++ src/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Login() {
+  return (
+    <>
+      <header>
+        <h1>Login Page</h1>
+      </header>
+      <main>
+        <h1>s</h1>
+      </main>
+    </>
+  );
+}
+
+export default Login;
