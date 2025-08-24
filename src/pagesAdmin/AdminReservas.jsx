1981766548acc05c52b7e0569d16e1071a2f5c40
julian carvajalino <juliancarvajalino8@gmail.com>
2025-08-24 13:49:00 -0500

    feat: error 404

--- src/pagesAdmin/AdminReservas.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdminReservas = () => {
-  return <div>Gestión de reservas</div>;
-};
-
-export default AdminReservas;
--- src/pagesAdmin/AdminUsuarios.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdminUsuarios = () => {
-  return <div>Gestión de usuarios</div>;
-};
-
-export default AdminUsuarios;
